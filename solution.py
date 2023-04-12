fb4d002575078bd8f82b9ad56d01c942864e5f82
Viktoriya Morozova <117348526+vickamorozova@users.noreply.github.com>
2023-04-12 08:16:57 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as stats
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 357282961 # Ваш chat ID, не меняйте название переменной
 
 def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return stats.ttest_ind(x, y)[1] < 0.08
